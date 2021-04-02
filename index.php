1665f3ca937793565c03c18fe1b2af712f98f7bc
hassasraf <hassasraf@gmail.com>
2021-04-02 21:24:21 +0600

    added style.css and modified index.php

--- index.php
+++ index.php
@@ -6,15 +6,96 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+	<link rel="stylesheet" href="style.css">
 
 </head>
 <body>
 	<div class="wrapper">
 	<form action="">
 	<i class="url-icon uil uil-link"></i>
-	<input type="text" placeholder="Enter or paste a lonk link">
+	<input type="text" placeholder="Enter or paste a lonk link" required>
 	<button>Shorten</button>
 	</form>
+
+	<div class="count">
+		<span class="total">
+			Total Links: 
+			<span>20</span> 
+			& Total Clicks: 
+			<span>144</span>
+		</span>
+		<a href="">Clear All</a>
+	</div>
+	
+	<div class="urls-area">
+		<div class="title">
+			<li>Shorten Url</li>
+			<li>Original URL</li>
+			<li>Clicks</li>
+			<li>Action</li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+		<div class="data">
+			<li>localhost/example</li>
+			<li>https://www.facebook.com/hass.asraf fdfdfd</li>
+			<li>300</li>
+			<li><a href="">Delete</a></li>
+		</div>
+	</div>
+	</div>
+
+	<div class="blur-effect"></div>
+	<div class="popup-box">
+		<div class="info-box">
+			Your shortlink is ready.You can also edit your short link now but can't edit once you saved it.
+		</div>
+		<form action="#">
+			<label for="edit">Edit Your shortlink</label>
+			<input type="text" value="example.com/xyz" spellcheck="false">
+			<i class="copy-icon uil uil-copy-alt"></i>
+			<button class="save-btn">Save</button>
+		</form>
 	</div>
 </body>
 </html>
